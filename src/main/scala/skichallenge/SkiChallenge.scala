c60f5bdda1da256c27d2bfb726492a15ed708b71
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2018-10-24 22:39:52 +0100

    Add auxiliary functions to compute local maxima

--- src/main/scala/skichallenge/SkiChallenge.scala
+++ src/main/scala/skichallenge/SkiChallenge.scala
@@ -33,4 +33,49 @@ object SkiChallenge {
     out
   }
 
+  /**
+    *
+    * @param resort Resort elevation map as a List of Lists of Int
+    * @param i1 Row of the starting map point
+    * @param j1 Column of the starting map point
+    * @param i2 Row of the ending map point
+    * @param j2 Column of the ending map point
+    * @return Return 1 when going down the slope, 0 when on a plane (including out of bounds) and -1 when going up
+    */
+  def slope(resort: List[List[Int]], i1: Int, j1: Int, i2: Int, j2: Int): Int = {
+
+    try{
+      if(resort(i1)(j1) > resort(i2)(j2)) 1
+      else if(resort(i1)(j1) < resort(i2)(j2)) -1
+      else 0
+    }catch{
+      case _: IndexOutOfBoundsException => 0
+    }
+  }
+
+  /**
+    *
+    * @param resort Resort elevation map as a List of Lists of Int
+    * @param i Row of the map point we want to compute neighboring slopes
+    * @param j Column of the map point we want to compute neighboring slopes
+    * @return List of Ints indicating the slope for each direction in the following order [West, South, East, North]
+    */
+  def getSlopes(resort: List[List[Int]], i: Int, j: Int): List[Int] = {
+    List(slope(resort, i, j, i,   j-1),
+         slope(resort, i, j, i+1, j),
+         slope(resort, i, j, i,   j+1),
+         slope(resort, i, j, i-1, j))
+  }
+
+  /**
+    *
+    * @param resort Resort elevation map as a List of Lists of Int
+    * @param i Row of the map point for which we want to determine if it is a local maximum (elevation peak)
+    * @param j Column of the map point for which we want to determine if it is a local maximum (elevation peak)
+    * @return Boolean clarifying whether the point is a local maximum or not based on its surrounding slopes
+    */
+  def isLocalMaximum(resort: List[List[Int]], i: Int, j: Int): Boolean = {
+    val slopes = this.getSlopes(resort, i, j)
+    !slopes.exists(_ < 0) && slopes.exists(_ > 0)
+  }
 }
--- src/test/scala/skichallenge/SkiChallengeTest.scala
+++ src/test/scala/skichallenge/SkiChallengeTest.scala
@@ -21,17 +21,95 @@ class SkiChallengeTest extends FunSuite with BeforeAndAfter {
                            List(4,4,1,6)))
   }
 
-  test("1 - Read map from File") {
+  test("2 - Read map from File") {
     val filePath = getClass.getResource("/4x4.txt").getPath
     val (rows, columns, resort) = SkiChallenge.getMapFromFile(filePath)
 
     assert(rows == 4)
     assert(columns == 4)
     assert(resort === List(List(4,8,7,3),
-      List(2,5,9,3),
-      List(6,3,2,5),
-      List(4,4,1,6)))
+                           List(2,5,9,3),
+                           List(6,3,2,5),
+                           List(4,4,1,6)))
   }
+
+  test("3 - Get list of slopes") {
+    val resort = List(List(4,8,7,3),
+                      List(2,5,9,3),
+                      List(6,3,2,5),
+                      List(4,4,1,6))
+
+    assert(SkiChallenge.getSlopes(resort, 0, 1) === List(1, 1, 1, 0))
+    assert(SkiChallenge.getSlopes(resort, 1, 2) === List(1, 1, 1, 1))
+    assert(SkiChallenge.getSlopes(resort, 2, 0) === List(0, 1, 1, 1))
+    assert(SkiChallenge.getSlopes(resort, 3, 1) === List(0, 0, 1, 1))
+    assert(SkiChallenge.getSlopes(resort, 3, 3) === List(1, 0, 0, 1))
+    assert(SkiChallenge.getSlopes(resort, 0, 0) === List(0, 1, -1, 0))
+    assert(SkiChallenge.getSlopes(resort, 2, 2) === List(-1, 1, -1, -1))
+    assert(SkiChallenge.getSlopes(resort, 3, 0) === List(0, 0, 0, -1))
+
+
+    val resort2 = List(List(8,8,8,8),
+                       List(8,8,8,8),
+                       List(8,8,5,8),
+                       List(8,8,8,8))
+
+    assert(SkiChallenge.getSlopes(resort2, 1, 2) === List(0, 1, 0, 0))
+    assert(SkiChallenge.getSlopes(resort2, 2, 1) === List(0, 0, 1, 0))
+    assert(SkiChallenge.getSlopes(resort2, 2, 3) === List(1, 0, 0, 0))
+    assert(SkiChallenge.getSlopes(resort2, 3, 2) === List(0, 0, 0, 1))
+    assert(SkiChallenge.getSlopes(resort2, 1, 1) === List(0, 0, 0, 0))
+    assert(SkiChallenge.getSlopes(resort2, 2, 2) === List(-1, -1, -1, -1))
+    assert(SkiChallenge.getSlopes(resort2, 3, 3) === List(0, 0, 0, 0))
+
+  }
+
+
+  test("4 - Check local maxima") {
+    val resort = List(List(4,8,7,3),
+                      List(2,5,9,3),
+                      List(6,3,2,5),
+                      List(4,4,1,6))
+
+    assert(SkiChallenge.isLocalMaximum(resort, 0, 1))
+    assert(SkiChallenge.isLocalMaximum(resort, 1, 2))
+    assert(SkiChallenge.isLocalMaximum(resort, 2, 0))
+    assert(SkiChallenge.isLocalMaximum(resort, 3, 1))
+    assert(SkiChallenge.isLocalMaximum(resort, 3, 3))
+
+    assert(!SkiChallenge.isLocalMaximum(resort, 0, 0))
+    assert(!SkiChallenge.isLocalMaximum(resort, 2, 2))
+    assert(!SkiChallenge.isLocalMaximum(resort, 3, 0))
+
+
+    val resort2 = List(List(8,8,8,8),
+                       List(8,8,8,8),
+                       List(8,8,5,8),
+                       List(8,8,8,8))
+
+    assert(SkiChallenge.isLocalMaximum(resort2, 1, 2))
+    assert(SkiChallenge.isLocalMaximum(resort2, 2, 1))
+    assert(SkiChallenge.isLocalMaximum(resort2, 2, 3))
+    assert(SkiChallenge.isLocalMaximum(resort2, 3, 2))
+
+    assert(!SkiChallenge.isLocalMaximum(resort2, 1, 1))
+    assert(!SkiChallenge.isLocalMaximum(resort2, 2, 2))
+    assert(!SkiChallenge.isLocalMaximum(resort2, 3, 3))
+
+
+    val resort3 = List(List(8,8,8,8),
+                       List(8,8,8,8),
+                       List(8,8,8,8),
+                       List(8,8,8,8))
+
+    for(i <- 0 until 4){
+      for(j <- 0 until 4){
+        assert(!SkiChallenge.isLocalMaximum(resort3, i, j))
+      }
+    }
+
+  }
+
 }
 
 
